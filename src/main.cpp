d5051c232ea5e65b5e4f82d88d14b9eb3eb75b10
variousideas <variousideas@naver.com>
2020-11-12 16:01:38 +0900

    도서 찾기 함수 테스트 코드
    main.cpp참고

--- src/main.cpp
+++ src/main.cpp
@@ -45,5 +45,28 @@ list<Book>::iterator find_writer(list<Book>& myList, string compare) {
 }
 
 int main() {
+    list<Book> bookList;
+    list<Book>::iterator it;
+
+    //* 이하 테스트
+    Book myBook("Title1", "Me1", "2");
+    bookList.push_back(myBook);
+    myBook = Book("Title2", "Me2", "2");
+    bookList.push_back(myBook);
+    myBook = Book("Title3", "Me3", "2");
+    bookList.push_back(myBook);
+
+    it = find_title_same(bookList, "Title1");
+    if (it == bookList.end())
+        cout << "찾는 도서가 없음." << endl;
+    else
+        cout << "도서 존재. 저자: " + (*it).getWriter() << endl;
+
+    it = find_title(bookList, "3");
+    if (it == bookList.end())
+        cout << "해당 문자열이 들어간 도서 없음." << endl;
+    else
+        cout << "해당 문자열이 들어간 도서 존재. 저자: " + (*it).getWriter() << endl;
+
     return 0;
 }
