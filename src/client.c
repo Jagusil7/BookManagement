3897f2e251a74ed073dd74b00f19e5dc39b0708e
variousideas <variousideas@naver.com>
2020-11-25 17:46:06 +0900

    ID를 서버로 전송하게 구현

--- src/client.c
+++ src/client.c
@@ -1,4 +1,3 @@
-// 서버-클라이언트 공통부분: 시작
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,61 +6,48 @@
 #include <sys/msg.h>
 #include <sys/types.h>
 
-#define MSG_TYPE_CALC 1
-#define MSG_TYPE_RSLT 2
-#define MSG_SIZE_CALC (sizeof(MsgCalc) - sizeof(long))
-#define MSG_SIZE_RSLT (sizeof(MsgRslt) - sizeof(long))
-
-struct __Calc {
-    int x;
-    int y;
-    char op;
-};
-typedef struct __Calc Calc;
-
 struct __MsgCalc {
     long mtype;
-    struct __Calc calc;
+    int data;
 };
 typedef struct __MsgCalc MsgCalc;
 
 struct __MsgRslt {
     long mtype;
-    int rslt;
+    int result;
 };
 typedef struct __MsgRslt MsgRslt;
 
 key_t mykey = 0;
 int msqid = 0;
-// 서버-클라이언트 공통부분: 끝
 
 int main(void) {
-    key_t mykey = ftok("mykey", 1); // 서버랑 같은 키 써야 함 주의.
-    int msquid = msgget(mykey, IPC_CREAT);
-
-    int x = 0;
-    int y = 0;
-    char op = 0;
-
     MsgCalc msgCalc;
     MsgRslt msgRslt;
 
-    puts("Input: [x] [op] [y]");
-    while (1) {
-        printf("<<");
-        scanf("%d %c %d", &x, &op, &y);
-        fflush(stdout);
-        fflush(stdin);
-        memset(&msgCalc, 0x00, sizeof(MsgCalc));
-        msgCalc.mtype = MSG_TYPE_CALC;
-        msgCalc.calc.x = x;
-        msgCalc.calc.y = y;
-        msgCalc.calc.op = op;
-        msgsnd(msqid, &msgCalc, MSG_SIZE_CALC, 0);
-
-        memset(&msgRslt, 0x00, sizeof(MsgRslt));
-        msgrcv(msqid, &msgRslt, MSG_SIZE_RSLT, MSG_TYPE_RSLT, 0);
-        printf(">> %d\n", msgRslt.rslt);
+    // 키생성 (path, 같은 경로에 대한 구분자)
+    mykey = ftok("mykey", 1);
+    if (mykey == -1) {
+        printf("ftok error");
+        return 0;
     }
+
+    // 메시지 큐 생성
+    msqid = msgget(mykey, IPC_CREAT | 0644);
+    if (msqid == -1) {
+        printf("msgget error");
+        return 0;
+    }
+
+    printf("enter your ID: ");
+    fflush(stdout);
+    int data;
+    scanf("%d", &data);
+    fflush(stdin);
+
+    memset(&msgCalc, 0x00, sizeof(MsgCalc));
+    msgCalc.mtype = 1;
+    msgCalc.data = data;
+    msgsnd(msqid, &msgCalc, sizeof(MsgCalc) - sizeof(long), 0);
     return 0;
 }
--- src/server.c
+++ src/server.c
@@ -1,4 +1,3 @@
-// 서버-클라이언트 공통부분: 시작
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,75 +6,52 @@
 #include <sys/msg.h>
 #include <sys/types.h>
 
-#define MSG_TYPE_CALC 1
-#define MSG_TYPE_RSLT 2
-#define MSG_SIZE_CALC (sizeof(MsgCalc) - sizeof(long))
-#define MSG_SIZE_RSLT (sizeof(MsgRslt) - sizeof(long))
-
-struct __Calc {
-    int x;
-    int y;
-    char op;
-};
-typedef struct __Calc Calc;
-
 struct __MsgCalc {
     long mtype;
-    struct __Calc calc;
+    int data;
 };
 typedef struct __MsgCalc MsgCalc;
 
 struct __MsgRslt {
     long mtype;
-    int rslt;
+    int result;
 };
 typedef struct __MsgRslt MsgRslt;
 
 key_t mykey = 0;
 int msqid = 0;
-// 서버-클라이언트 공통부분: 끝
 
 void signalHandler(int signum) {
     if (signum == SIGINT) {
         msgctl(msqid, IPC_RMID, NULL);
-        exit(0);
-    }
-}
-int calculate(Calc calc) {
-    switch (calc.op) {
-    case '+':
-        return calc.x + calc.y;
-    case '-':
-        return calc.x - calc.y;
-    case '*':
-        return calc.x * calc.y;
-    case '/':
-        return calc.x / calc.y;
-    default:
-        return 0;
     }
 }
 
-int main(int argc, char const *argv[]) {
+int main(void) {
     MsgCalc msgCalc;
     MsgRslt msgRslt;
+
+    // 키생성 (path, 같은 경로에 대한 구분자)
     mykey = ftok("mykey", 1);
-    msqid = msgget(mykey, IPC_CREAT | 0644);
-    signal(SIGINT, signalHandler);
-    while (1) {
-        puts("Wait");
-        memset(&msgCalc, 0x00, sizeof(MsgCalc));                  // 초기화
-        msgrcv(msqid, &msgCalc, MSG_SIZE_CALC, MSG_TYPE_CALC, 0); // 받기
-        printf("Receive: %d %c %d\n", msgCalc.calc.x, msgCalc.calc.op,
-               msgCalc.calc.y);
-
-        memset(&msgRslt, 0x00, sizeof(MsgRslt));
-
-        msgRslt.mtype = MSG_TYPE_RSLT;
-        msgRslt.rslt = calculate(msgCalc.calc);
-        msgsnd(msqid, &msgRslt, MSG_SIZE_RSLT, 0);
-        printf("Send: %d\n", msgRslt.rslt);
-        fflush(stdout);
+    if (mykey == -1) {
+        printf("ftok error");
+        return 0;
     }
+
+    // 메시지 큐 생성
+    msqid = msgget(mykey, IPC_CREAT | 0644);
+    if (msqid == -1) {
+        printf("msgget error");
+        return 0;
+    }
+
+    signal(SIGINT, signalHandler); // 뭔데
+
+    puts("wait.");
+    memset(&msgCalc, 0x00, sizeof(MsgCalc));
+    msgrcv(msqid, &msgCalc, sizeof(MsgCalc) - sizeof(long), 1, 0);
+    printf("ID: %d\n", msgCalc.data);
+
+    fflush(stdout);
     return 0;
 }
